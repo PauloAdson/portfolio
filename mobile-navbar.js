ecf48e3db2fecc0a2cbf44915aa1b0d5c3fd3685
Paulo Adson <paulo.alves200581@gmail.com>
2024-03-13 12:08:23 -0300

    Alterando o código do JS para ficar mais intuitivo para o cliente, e também tirando o bug de sumir os links

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -1,3 +1,95 @@
+// class MobileNavbar {
+//     constructor(mobileMenu, navList, navLinks) {
+//         this.mobileMenu = document.querySelector(mobileMenu);
+//         this.navList = document.querySelector(navList);
+//         this.navLinks = document.querySelectorAll(navLinks);
+//         this.activeClass = "active";
+
+//         // this.handleClick = this.handleClick.bind(this);
+//         this.handleToggle = this.handleToggle.bind(this); // Fechar o menu através dos links
+//         this.handleLinkClick = this.handleLinkClick.bind(this); // Fechar o menu através dos links
+
+//         this.handleDocumentClick = this.handleDocumentClick.bind(this);
+//     }
+
+
+//     // Fechar o menu através dos links
+//     handleToggle() {
+//         this.navList.classList.toggle(this.activeClass);
+//         this.mobileMenu.classList.toggle(this.activeClass);
+//         this.animateLinks();
+//     }
+
+//     handleLinkClick(event) {
+//         // Impede que o clique no link propague para o documento
+//         event.stopPropagation();
+
+//         if (this.navList.classList.contains(this.activeClass)) {
+//             this.handleToggle();
+//         }
+        
+//     } // Termina aqui
+
+//     handleDocumentClick(event) {
+//         // Verifica se o clique foi fora do menu
+//         if (!this.mobileMenu.contains(event.target)) {
+//             this.navList.classList.remove(this.activeClass);
+//             this.mobileMenu.classList.remove(this.activeClass);
+//             this.animateLinks();
+//         }
+//     } //
+
+
+//     animateLinks() {
+//         this.navLinks.forEach((link, index) => {
+//             link.style.animation
+//             ? (link.style.animation = "")
+//             : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
+//         });
+//     }
+
+//     ////Código antes dele fechar através dos links
+
+//     // handleClick() {
+//     //     this.navList.classList.toggle(this.activeClass);
+//     //     this.mobileMenu.classList.toggle(this.activeClass);
+//     //     this.animateLinks();
+//     // }
+
+//     // addClickEvent() {
+//     //     this.mobileMenu.addEventListener("click", this.handleClick);
+//     // }
+
+
+//     addEventListeners() {
+//         this.mobileMenu.addEventListener("click", this.handleToggle);
+
+//         this.navLinks.forEach(link => {
+//             link.addEventListener("click", this.handleLinkClick);
+//         });
+
+//         document.addEventListener("click", this.handleDocumentClick);
+//     }
+
+
+//     init() {
+//         if (this.mobileMenu) {
+//             this.addEventListeners();
+//             // this.addClickEvent();
+//         }
+//         return this;
+//     }
+// }
+
+// const mobileNavbar = new MobileNavbar(
+//     ".mobile-menu",
+//     ".nav-list",
+//     ".nav-list li",
+// );
+// mobileNavbar.init();
+
+
+
 class MobileNavbar {
     constructor(mobileMenu, navList, navLinks) {
         this.mobileMenu = document.querySelector(mobileMenu);
@@ -31,13 +123,14 @@ class MobileNavbar {
     } // Termina aqui
 
     handleDocumentClick(event) {
-        // Verifica se o clique foi fora do menu
-        if (!this.mobileMenu.contains(event.target)) {
+        // Verifica se o menu está ativo antes de lidar com o clique fora do menu
+        if (this.mobileMenu.classList.contains(this.activeClass) && !this.mobileMenu.contains(event.target)) {
             this.navList.classList.remove(this.activeClass);
             this.mobileMenu.classList.remove(this.activeClass);
             this.animateLinks();
         }
-    } //
+    }
+    
 
 
     animateLinks() {
@@ -47,19 +140,6 @@ class MobileNavbar {
             : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
         });
     }
-
-    ////Código antes dele fechar através dos links
-
-    // handleClick() {
-    //     this.navList.classList.toggle(this.activeClass);
-    //     this.mobileMenu.classList.toggle(this.activeClass);
-    //     this.animateLinks();
-    // }
-
-    // addClickEvent() {
-    //     this.mobileMenu.addEventListener("click", this.handleClick);
-    // }
-
 
     addEventListeners() {
         this.mobileMenu.addEventListener("click", this.handleToggle);
