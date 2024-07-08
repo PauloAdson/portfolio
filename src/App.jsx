aae42595f0e4d6ac03f0dba301aef93150793f94
Paulo Adson <paulo.alves200581@gmail.com>
2024-07-08 19:50:03 -0300

    adicionando os componentes de contato e obrigado

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Footer } from './components/Footer/Footer';
 import { Home } from './components/Home/Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Contato } from './components/Contato/Contato';
+import { Obrigado } from './components/Obrigado/Obrigado';
 
 class App extends React.Component {
   render() {
@@ -15,6 +16,7 @@ class App extends React.Component {
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/contato' element={<Contato />} />
+            <Route path='/obrigado' element={<Obrigado />} />
           </Routes>
         </BrowserRouter>
         <Footer />
---
+++ src/components/Obrigado/Obrigado.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export class Obrigado extends React.Component {
+    render() {
+        return (
+            <main>
+                <div class="content-obrigado">
+                    <h1 class="h1-obrigado">Obrigado pelo contato!</h1>
+                    <p class="p-obrigado">Em breve estarei respondendo</p>
+                    <a class="btn-voltar-ao-inicio" href="./">Voltar ao ínicio</a>
+                </div>
+            </main>
+        );
+    }
+}
